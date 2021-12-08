c46997bf7bc783e2d7d46fb1a41472c70d417e0c
Raviraj Dulange <redhood.dumb@gmail.com>
2021-12-09 00:12:37 +0530

    Make port env var

--- app.js
+++ app.js
@@ -178,6 +178,6 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("App running on port 3000");
 });
