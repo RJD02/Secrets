c0de4eeab0084cd4975568e54af3a2fa6d37a59a
Raviraj Dulange <redhood.dumb@gmail.com>
2021-12-08 23:57:23 +0530

    Set the new hosted url

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL:
+        "https://evening-river-18409.herokuapp.com/auth/google/secrets",
     },
     (accessToken, refreshToken, profile, cb) => {
       console.log(profile);
